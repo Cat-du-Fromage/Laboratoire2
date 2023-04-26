05aab09e9a3136a576e482a3acde929b4795fe69
Cat-du-Fromage <46396184+Cat-du-Fromage@users.noreply.github.com>
2023-04-26 16:17:04 +0200

    class .c

---
+++ listes_dynamiques.c
@@ -0,0 +1,22 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : listes_dynamiques.h
+ Auteur(s)      : Johan Voland, Florian Duruz, Guillaume Dunant
+ Date creation  : 26.04.2023
+
+ Description    : Librairie permettant la gestion de listes doublement chaînées
+                  non circulaires
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 gcc 12.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include "listes_dynamiques.h"
+#include <stdio.h>
+
+int main() {
+   printf("Hello, World!\n");
+   return 0;
+}
